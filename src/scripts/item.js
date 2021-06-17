5831b78668a1927a1a81e288a55007c91df5d519
YuriiTrofimenko <tyaa@ukr.net>
2021-06-17 16:17:18 +0300

    google firebase auth

--- src/scripts/item.js
+++ src/scripts/item.js
@@ -1,16 +1,10 @@
 export default class TodoItemModel {
-  // static lastId = 0
-  constructor(title, details, date, done, id) {
-    /* if (!id) {
-      this.id = ++TodoItemModel.lastId
-    } else {
-      this.id = id
-    } */
+  constructor(title, details, date, userId, done, id) {
     this.id = id
     this.title = title
     this.details = details
     this.date = date
+    this.userId = userId
     this.done = done
-    // console.log(this)
   }
 }
